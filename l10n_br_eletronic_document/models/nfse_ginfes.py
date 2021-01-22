65ed8bd121fc12cfe6bfdfb97bf578068c646dd3
Álvaro  Rios <ninobaldo@gmail.com>
2021-01-22 15:10:58 -0300

    Fix breakline

--- l10n_br_eletronic_document/models/nfse_ginfes.py
+++ l10n_br_eletronic_document/models/nfse_ginfes.py
@@ -68,7 +68,7 @@ def _convert_values(vals):
         if rps['itens_servico'][0]['cnae_servico']:
             rps['cnae_servico'] = rps['itens_servico'][0]['cnae_servico']
 
-        rps['descricao'] = rps['observacoes'] or rps['discriminacao'].replace('|', '\n')
+        rps['descricao'] = rps['observacoes'].replace("\n", '&#xD;&#xA;') or rps['discriminacao'].replace('|', '&#xD;&#xA;')
         
         # Codigo do municipio do IBGE
         rps['codigo_municipio'] = rps['emissor']['codigo_municipio']
